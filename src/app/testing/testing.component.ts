51c2e3ed02b7d41f1e74eb9d94332b1b14c60d72
Joseph Kamemba <kamembajose07@gmail.com>
2024-11-27 07:59:37 +0300

    more changes on data binding

--- src/app/testing/testing.component.ts
+++ src/app/testing/testing.component.ts
@@ -10,7 +10,19 @@ import { CommonModule } from '@angular/common';
 export class TestingComponent {
   title = 'Hello Julls, I love You Soo Much';
   isVisible = true;
-  items: string[]=['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+  items: string[]=['Prctice Self-Care', 'Process Your Emotions',
+     'Focus on Hobbies and Interests', 'Build a Support Network', 
+     'Practive Mindfulness and Relaxation', 'Establish a Routine', 
+     'Avoid Unhealthy Coping Mechanisms', 
+     'Practice Forgiveness and Letting go',
+      'Connect with Nature', 'Celebrate Small Wins'];
+
+  message: string = '';
+
+  onButtonClick(): void {
+    this.message = 'I love you Baby Boo'
+  }
+
 
   changeTitle() {
     this.title = 'Title Has Been Changed'
